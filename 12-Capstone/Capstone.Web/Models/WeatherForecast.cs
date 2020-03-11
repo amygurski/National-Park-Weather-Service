1d198256cc6c017c89b407aacda24c65f8bcca38
Amy Gurski <amygurski@gmail.com>
2020-03-11 13:34:06 -0400

    Adds weather forecast to detail page

--- 12-Capstone/Capstone.Web/Models/WeatherForecast.cs
+++ 12-Capstone/Capstone.Web/Models/WeatherForecast.cs
@@ -12,49 +12,58 @@ namespace Capstone.Web.Models
         public int Low { get; set; }
         public int High { get; set; }
         public string Forecast { get; set; }
+        public string ImageName
+        {
+            get
+            {
+                //remove spaces from forecast to get image name
+                string image = Forecast.Replace(" ", String.Empty);
+                return image;
+            }
+        }
 
-        //public string WeatherRecommendation
-        //{
-        //    get
-        //    {
-        //        switch (Forecast)
-        //        {
-        //            case "snow":
-        //                return "Pack snowshoes.";
-        //            case "rain":
-        //                return "Pack rain gear and wear waterproof shoes.";
-        //            case "thunderstorms":
-        //                return "Seek shelter and avoid hiking on exposed ridges.";
-        //            case "sun":
-        //                return "Pack sunblock.";
-        //            default:
-        //                return null;
-        //        }
-        //    }
-        //}
-        //public string TemperatureRecommendation
-        //{
-        //    get
-        //    {
-        //        if (High > 75 && High-Low > 20)
-        //        {
-        //            return "Bring an extra gallon of water and wear breathable layers.";
-        //        }
-        //        else if (High > 75)
-        //        {
-        //            return "Bring an extra gallon of water.";
-        //        }
-        //        else if (Low < 20 && High-Low > 20)
-        //        {
-        //            return "Wear breathable layers. Danger: Exposure to frigid temperatures.";
-        //        }
-        //        else if (Low < 20)
-        //        {
-        //            return "Danger: Exposure to frigid temperatures.";
-        //        }
+        public string WeatherRecommendation
+        {
+            get
+            {
+                switch (Forecast)
+                {
+                    case "snow":
+                        return "Pack snowshoes.";
+                    case "rain":
+                        return "Pack rain gear and wear waterproof shoes.";
+                    case "thunderstorms":
+                        return "Seek shelter and avoid hiking on exposed ridges.";
+                    case "sun":
+                        return "Pack sunblock.";
+                    default:
+                        return null;
+                }
+            }
+        }
+        public string TemperatureRecommendation
+        {
+            get
+            {
+                string recommendation = null;
 
-        //        return null;
-        //    }
-        //}
+                if (High > 75)
+                {
+                    recommendation += "Bring an extra gallon of water. ";
+                }
+
+                if (High - Low > 20)
+                {
+                    recommendation += "Wear breathable layers. ";
+                }
+
+                if (Low < 20)
+                {
+                    return "Danger: Exposure to frigid temperatures. ";
+                }
+
+                return recommendation;
+            }
+        }
     }
 }
